f705402fc62ff5cb53bf7dde561385e888d47378
root <root@forum.bollywoodmasti.tv>
2017-02-09 06:31:11 +0000

    testing after changing config to dummy

---
+++ config_change_back_to_config.php
@@ -0,0 +1,21 @@
+<?php return array (
+  'debug' => false,
+  'database' => 
+  array (
+    'driver' => 'mysql',
+    'host' => 'localhost',
+    'database' => '{db_name}',
+    'username' => '{add_username}',
+    'password' => '{password}',
+    'charset' => 'utf8mb4',
+    'collation' => 'utf8mb4_unicode_ci',
+    'prefix' => '{forum_name}',
+    'strict' => false,
+  ),
+  'url' => '{url}',
+  'paths' => 
+  array (
+    'api' => 'api',
+    'admin' => 'admin',
+  ),
+);
